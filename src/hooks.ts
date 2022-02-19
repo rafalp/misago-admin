891fda8ae3c1ce27900ea5e3c2b2969bc61892b3
rafalp <kontakt@rpiton.com>
2022-02-19 15:47:03 +0100

    Plugin setup

---
+++ src/hooks.ts
@@ -0,0 +1,9 @@
+import React from "react"
+
+const EXTRA_COMPONENTS: Record<string, Array<React.ComponentType>> = {
+  TEST: []
+}
+
+export {
+  EXTRA_COMPONENTS
+}
---
+++ src/plugins/index.ts
@@ -0,0 +1,3 @@
+const setupPlugins = () => {}
+
+export default setupPlugins
